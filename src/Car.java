bee1dd76c2b683a63eb20a7643ad3325c70236af
SergeiH1986 <zwark2005god@mail.ru>
2023-06-07 03:25:05 +0500

    proga

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,10 @@
 public class Car {
     String brand;
     int hoursPower;
+    int hoursPower;
+    int hoursPower;
+
+
     boolean isawd;
     void start(){
         System.out.println(brand +"startuet");
