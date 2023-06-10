e91de88bdedbdd24c3cc357b747e6d9f03e89507
SergeiH1986 <zwark2005god@mail.ru>
2023-06-11 01:25:41 +0500

    zadacha 1

---
+++ Slova.java
@@ -0,0 +1,7 @@
+public class Slova {
+    //Metod dlya vypekaniya
+    public void bake() {
+        //Vyvodim tekst v konsol'
+        System.out.println("Slova 1");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        Car mazda = new Car();
-        mazda.brand = "mazda";
-        mazda.hoursPower = 198;
-        mazda.isawd = false;
-        mazda.start();
+        int[] nums = new int[10];
+        for (int i=0; i< nums.length;i++){
+            System.out.println(nums[i]);
+        }
+
     }
 }
