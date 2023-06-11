b8170002835ca564bd596eb6ae7f0f3cb2c69370
SergeiH1986 <zwark2005god@mail.ru>
2023-06-12 03:36:56 +0500

    vizitaie

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] nums = new int[10];
-        for (int i=0; i< nums.length;i++){
-            System.out.println(nums[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(reader.readLine());
+        int j = Integer.parseInt(reader.readLine());
+
+        System.out.println(i-j);
 
         }
 
     }
-}
