e0126bcdb47a7575f6dc662a1fed40055ad58b86
SergeiH1986 <zwark2005god@mail.ru>
2023-06-12 04:20:08 +0500

    peremeie vvedei

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        System.out.println("Введите первое число");
         int i = Integer.parseInt(reader.readLine());
-
+        System.out.println("Введите второе число");
         int j = Integer.parseInt(reader.readLine());
-
+        System.out.println("Сумма чисел равна ");
         System.out.println(i+j);
 
         }
