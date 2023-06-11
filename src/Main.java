905843ae3ebd35d0db5f49a2757fd1a6b8146e01
SergeiH1986 <zwark2005god@mail.ru>
2023-06-12 04:02:39 +0500

    proeli

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int i = Integer.parseInt(reader.readLine());
+
         int j = Integer.parseInt(reader.readLine());
 
         System.out.println(i+j);
